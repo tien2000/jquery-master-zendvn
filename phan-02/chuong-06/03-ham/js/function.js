b173e07bb7e67ac1272f771305f315140f904f71
tien <tienls@fastboy.net>
2017-12-11 15:10:20 +0700

    first commit

---
+++ phan-02/chuong-06/03-ham/js/function.js
@@ -0,0 +1,11 @@
+// showInfo
+
+function showInfo(user, job){
+    document.write("Tên: " + user + "<br>");
+    document.write("Job: " + job + "<br>");
+}
+
+function showFullName(firstname, lastname){
+    var fullName = firstname + lastname;
+    return fullName;
+}
