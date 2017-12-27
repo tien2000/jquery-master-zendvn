cd20d7dead0d18ca918b514244ad3762819d6a66
tien <tienls@fastboy.net>
2017-12-27 20:04:59 +0700

    Chương 9: DOM: HTMLElement:: 5-9

--- phan-02/chuong-09/06-htmlelement/js/myCssElement-02.js
+++ phan-02/chuong-09/06-htmlelement/js/myCssElement-02.js
@@ -29,6 +29,10 @@ this.myCssElement = function(objHTML){
         }
     }
 
+    this.cssStyle = function(){
+        return getStyleElement(objHTML);
+    }
+
     /* ===============================
         PRIVATE
     ================================== */
