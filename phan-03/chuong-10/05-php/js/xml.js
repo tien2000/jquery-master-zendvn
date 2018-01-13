63071c7d42dd493c18fe2388f18aa0acd7ee7c79
tien <tienls@fastboy.net>
2018-01-13 20:11:25 +0700

    Chương 10: Javascript & XML: XML & PHP: Connect DB.

---
+++ phan-03/chuong-10/05-php/js/xml.js
@@ -0,0 +1,36 @@
+function loadFile(url, method, type){
+    // Khởi tạo đối tượng XMLHttpRequest
+    if (window.XMLHttpRequest) {
+        var xHttp = new XMLHttpRequest;
+    } else{
+        var xHttp = new ActiveXObject("Microsoft.XMLHTTP");     // Chạy trên iE
+    }
+
+    // Xác định phương thức và tập tin muốn lấy thông tin.
+    // Phương thức GET - POST
+    xHttp.open(method, url, false);
+
+    // Gửi yêu cầu đến máy chủ.
+    xHttp.send();
+
+    // Lấy nội dung trả về từ máy chủ.
+    if (type == "text") {
+        var val = xHttp.responseText;
+    } else{
+        var val = xHttp.responseXML;
+    }
+
+    return val;
+}
+
+function getNode(parent, tageName){
+    return parent.getElementsByTagName(tageName)[0];
+}
+
+function getNodeValue(node){
+    return node.childNodes[0].nodeValue;
+}
+
+function getAttributeNode(node, attrName){
+    return node.getAttribute(attrName);
+}
