b5319a2e725fb83e7c3d4d88979b2c59b8c0f742
tien <tienls@fastboy.net>
2018-01-26 19:00:36 +0700

    Chương 11: JQuery toàn tập: Ajax:: Shorthand - get.

---
+++ phan-03/chuong-11/08-ajax/files/file-002a.php
@@ -0,0 +1,9 @@
+<?php
+    $info = array();
+    $info["name"] = "TienLS";
+    $info["email"] = "neotien200065@gmail.com";
+    $info["website"] = "zendvn.com";
+
+    $jsonString = json_encode($info);
+    echo $jsonString;
+?>
