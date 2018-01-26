687850f0b850f9bb7ca8e3530a0835bad240cad0
tien <tienls@fastboy.net>
2018-01-26 20:05:35 +0700

    Chương 11: JQuery toàn tập: Ajax:: Shorthand - post 01

---
+++ phan-03/chuong-11/08-ajax/files/file-003.php
@@ -0,0 +1,18 @@
+
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<?php
+    // GET | POST
+    echo "Nhận dữ liệu từ phương thức GET" . "<br>";
+    echo "==========================" . "<br>";
+    echo "<pre>";
+    echo print_r($_GET);
+    echo "</pre>";
+    echo "==========================" . "<br>";
+
+    echo "Nhận dữ liệu từ phương thức POST" . "<br>";
+    echo "==========================" . "<br>";
+    echo "<pre>";
+    echo print_r($_POST);
+    echo "</pre>";
+    echo "==========================" . "<br>";
+?>
