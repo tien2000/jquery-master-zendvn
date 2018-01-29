d94a4fb22be3d6d2d127264aaeede46c82ccdc83
tien <tienls@fastboy.net>
2018-01-29 19:20:10 +0700

    Edit some file.

--- phan-03/chuong-11/08-ajax/files/file-002a.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $info = array();
-    $info["name"] = "TienLS";
-    $info["email"] = "neotien200065@gmail.com";
-    $info["website"] = "zendvn.com";
-
-    $jsonString = json_encode($info);
-    echo $jsonString;
-?>
