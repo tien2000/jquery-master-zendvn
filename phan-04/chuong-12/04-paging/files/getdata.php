188667ae6d78cb0f7f020268433d62b350a76438
TienLS <neotien200065@gmail.com>
2018-03-02 19:30:47 +0700

    Chương 12: Ứng dụng với JQuery: Ứng dụng 4:: Paging 5-8./.

--- phan-04/chuong-12/04-paging/files/getdata.php
+++ phan-04/chuong-12/04-paging/files/getdata.php
@@ -10,14 +10,13 @@
 	//Lay tong so trang trong database
 	//=============================================
 	if($type == "count"){
-		$items = (int)$_GET["items"];	
-		
+		$items = (int)$_GET["items"];
 		$sql = 'SELECT COUNT(id) 
 				FROM `products` 
 				WHERE `status` = 1';
 				
 		$result 	= mysqli_query($conn, $sql);
-		$totalItem 		= mysqli_fetch_row($result); 
+		$totalItem 	= mysqli_fetch_row($result); 
 		$pages		= $totalItem[0]/$items;
 		$total 		= array("total" => 0);
 
@@ -32,14 +31,75 @@
 		$total["total"] = $pages;
 
 		echo json_encode($total);
+	}
 
+	//====================================================
+	//Lấy các phần tử tương ứng trong trang hiện thời.
+	//====================================================
+	if($type == "list"){
 		// echo "<pre>";
-		// print_r($tmp);
+		// print_r($_POST);
 		// echo "</pre>";
 
+		// LIMIT offet, items
+		// page: 1 => 0, 6
+		// page: 2 => 6, 6
+		// page: 3 => 12, 6
+		
+		$items 			= (int)$_POST["items"];
+		$page 			= (int)$_POST["page"];
+		$offset 		= ($page - 1) * $items;
+		
+		$sql = "SELECT `id`, `name`
+				FROM `products`
+				WHERE `status` = 1
+				ORDER BY `id` ASC				
+				LIMIT " . $offset . "," . $items;
+
+		$result = mysqli_query($conn, $sql);
+		$book = array();
+
+		while ($row = mysqli_fetch_assoc($result)) {
+			$book[] = $row;
+		}
+
+		echo json_encode($book);
+
 		// echo "<pre>";
-		// print_r($pages);
+		// print_r($book);
 		// echo "</pre>";
+	}
+
+	//====================================================
+	//Lấy 1 phần tử đứng sau id cuối cùng.
+	//====================================================
+	if($type == "one"){
+		$lastID = (int)$_GET["id"];
+
+		$sql = "SELECT `id`, `name`
+				FROM `products`
+				WHERE `status` = 1
+				AND `id` > ". $lastID ."	
+				LIMIT 1";
+
+		$result = mysqli_query($conn, $sql);
+		$book = array();
+		$book = mysqli_fetch_assoc($result);
+
+		echo json_encode($book);
+	}
+
+	//====================================================
+	//Delete 1 phần tử.
+	//====================================================
+	if($type == "delete"){
+		$itemID = (int)$_GET["id"];
+
+		$sql = "DELETE
+				FROM `products`
+				WHERE `id` = ". $itemID;
+
+		$result = mysqli_query($conn, $sql);
 	}
 ?>
 
--- phan-04/chuong-12/04-paging/js/paging.js
+++ phan-04/chuong-12/04-paging/js/paging.js
@@ -28,11 +28,14 @@
 		var btnNext 		= $(options.btnNext);
 		var txtCurrentPage 	= $(options.txtCurrentPage);
 		var lblPageInfo 	= $(options.lblPageInfo);
+
+		var aRows			= '';
 	// ============================================================
 
 	// ============================================================
 		// Khởi tạo các hàm cần thiết khi Plugin được sử dụng
-		init();				
+		init();		
+		setRowsHeight();		
 	// ============================================================
 
 	// ============================================================
@@ -47,7 +50,7 @@
 			}).done(function(data){				
 				options.total = data.total;	
 				pageInfo();
-				// console.log(options);
+				loadData(options.currentPage);
 			});
 
 			// Gán sự kiện cho btnPrevious, btnNext, txtcurrentPage
@@ -55,10 +58,12 @@
 
 			btnPrevious.on("click", function(e){
 				goPrevious();
+				e.stopImmediatePropagation();
 			});
 
 			btnNext.on("click", function(e){
 				goNext();
+				e.stopImmediatePropagation();
 			});
 
 			txtCurrentPage.on("keyup", function(e){
@@ -70,7 +75,8 @@
 					} else {
 						loadData(currentPageValue);
 						options.currentPage = currentPageValue;
-						pageInfo();
+						pageInfo();		
+						loadData(options.currentPage);				
 					}
 
 					// console.log(currentPageValue);
@@ -82,12 +88,26 @@
 
 		// ==== Hàm xử lý khi nhấn nút btnPrevious ====
 		function goPrevious(){
-
+			console.log("goPrevious: " + options.currentPage);
+			if (options.currentPage != 1) {
+				var pre = (options.currentPage) - 1;
+				loadData(pre);
+				setCurrentPage(pre);
+				options.currentPage = pre;
+				pageInfo(pre);
+			}
 		}
 
 		// ==== Hàm xử lý khi nhấn nút btnNext ====
 		function goNext(){
-
+			console.log("goNext: " + options.currentPage);
+			if (options.currentPage != options.total) {
+				var pre = (options.currentPage) + 1;
+				loadData(pre);
+				setCurrentPage(pre);
+				options.currentPage = pre;
+				pageInfo(pre);
+			}
 		}
 
 		// ==== Hàm xử lý gán giá trị truyền vào trogn ô textbox txtcurrentPage ====
@@ -102,24 +122,100 @@
 
 		// ==== Thiết lập chiều cao cho ul#rows ====
 		function setRowsHeight(){
-			
+			var ulHeight = (options.items * options.height) + "px";
+			rows.css("height", ulHeight);
 		}
 
 		// ==== Hàm load thông tin từ DB đưa vào #rows ====
 		function loadData(page){
 			// console.log("loadData");
+
+			$.ajax({
+				url			: "files/getdata.php?type=list",
+				type		: "POST",
+				dataType	: "json",
+				cache		: "false",
+				data		: {
+					"items" 		: options.items,
+					"page"			: page
+				},
+			}).done(function(data){
+				// console.log(data);
+
+				if (data.length > 0) {
+					rows.empty();
+
+					$.each(data, function (i, val){ 
+						var str = "<li item-id="+ val.id +">"
+									+ val.id + ". " + val.name 
+									+ "<a href='#'>Xóa</a>"
+									+"</li>";
+						rows.append(str);
+					});
+
+					// Lấy tập hợp các thẻ <a> nằm trong "ul#rows li"
+					aRows = options.rows + " li a";
+					// console.log(aRows);
+
+					$(aRows).on("click", function (e){ 
+						deleteItem(this);
+					});
+				}
+			});
 		}
 
 		// ==== Xóa 1 dòng trên giao diện ====
 		function deleteItem(obj){
-			
+			var parent = $(obj).parent();
+			var itemID = $(parent).attr("item-id");
+			var lastID = $(rows).children(":last").attr("item-id");
+			// console.log(lastID);
+
+			// Ẩn và xóa phần tử li được nhấn.
+			$(parent).fadeOut({
+				duration		: "slow",
+				done			: function () { 
+					$.ajax({
+						url 		: "files/getdata.php?type=delete&id=" + itemID,
+						type		: "GET",
+						dataType	: "json"
+					});
+
+					// if (itemID == lastID && $(rows).children().length == 1) {		//itemID == lastID: Điều kiện ko tối ưu.
+					// 	options.currentPage = options.currentPage - 1;
+					// }
+
+					if ($(rows).children().length == 1) {
+						options.currentPage = options.currentPage - 1;
+					}
+
+					init();
+					// pageInfo();
+					$(this).remove();			// Loại bỏ hoàn toàn phần tử trong mã nguồn.
+				 }
+			});
+
+			$.ajax({
+				url 		: "files/getdata.php?type=one&id=" + lastID,
+				type		: "GET",
+				dataType	: "json"
+			}).done(function(data) { 
+				// console.log(data);
+				if (data != false) {
+					var str = '<li item-id="' + data.id + '">' 
+								+ data.id + ' - ' + data.name 
+								+ '<a href="#">Xoa</a>' 
+								+ '</li>';
+					str = $(str).hide().appendTo(rows);
+					$(str).fadeIn("slow");
+				}				
+			});
 		}
 	// ============================================================
 	};
 })(jQuery);
 
 $(document).ready(function(e){
-	var obj = {};
-
+	var obj = {"items" : 4};
 	$("#paging").tPaging(obj);
 });
