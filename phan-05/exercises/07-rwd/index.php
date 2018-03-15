f6132e1a0aee2c824372f4a0041da0840782630c
TienLS <neotien200065@gmail.com>
2018-03-15 18:59:15 +0700

    Chương 13: rwd:: Navigation 01.

---
+++ phan-05/exercises/07-rwd/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Business LTD</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet/less" type="text/css" media="screen" href="_/components/less/style.css" />
+    
+    <script type="text/javascript" src="js/jquery-1.10.0.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/less-1.7.0.min.js"></script>
+</head>
+<body>
+    <section class="headerSection">
+        <?php
+            include_once '_/components/php/navigation.php';
+        ?>
+    </section>
+</body>
+</html>
