b6c0f6fe85734a5c5dc5857d5b43f76249a5ca6e
TienLS <neotien200065@gmail.com>
2018-03-18 08:05:53 +0700

    Chương 13: rwd:: What we do 1-2.

---
+++ phan-05/exercises/07-rwd/_/components/php/field.php
@@ -0,0 +1,68 @@
+<div class="container">
+    <div class="row title">
+        <h3>What we do???</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci fugit unde quisquam quasi aspernatur sequi, error nobis aliquid accusamus dignissimos.</p>        
+    </div>
+    <div class="row">
+        <div class="col-md-2">
+            <div class="well">
+                <h4 id="popover-1" data-toggle="popover" title="Design" data-content="> The Best <br> > Bester <br> > Good">
+                    <i class="fa fa-magic"></i>
+                    <span>Design</span>
+                </h4>
+                <a href="#">View Detail</a>
+                <div class="popover-title hidden"><h3>ABC</h3></div>
+                <ul class="popover-content hidden">
+                    <li>123</li>
+                    <li>456</li>
+                    <li>789</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well">
+                <h4 id="popover-2">
+                    <i class="fa fa-link"></i>
+                    <span>Design</span>
+                </h4>
+                <a href="#">View Detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well">
+                <h4>
+                    <i class="fa fa-bullseye"></i>
+                    <span>Design</span>
+                </h4>
+                <a href="#">View Detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well">
+                <h4>
+                    <i class="fa fa-bug"></i>
+                    <span>Design</span>
+                </h4>
+                <a href="#">View Detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well">
+                <h4>
+                    <i class="fa fa-eye"></i>
+                    <span>Design</span>
+                </h4>
+                <a href="#">View Detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well">
+                <h4>
+                    <i class="fa fa-eye-slash"></i>
+                    <span>Design</span>
+                </h4>
+                <a href="#">View Detail</a>
+            </div>
+        </div>
+    </div>
+</div>
--- phan-05/exercises/07-rwd/index.php
+++ phan-05/exercises/07-rwd/index.php
@@ -7,10 +7,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <link rel="stylesheet/less" type="text/css" media="screen" href="_/components/less/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="_/components/less/font-awesome.min.css" />
     
     <script type="text/javascript" src="js/jquery-1.10.0.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/less-1.7.0.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function(e){
+            $('[data-toggle=popover]').popover({
+                "html"              : true,
+                "animation"         : true,
+                "placement"         : "top",
+                "trigger"           : "hover"
+            }).on('shown.bs.popover', function () {
+                var pos = parseInt($(".popover").css("top")) - 22 + "px";
+
+                $(".popover").css("top", pos);
+            });
+
+            $("#popover-2").popover({
+                "html"              : true,
+                "animation"         : true,
+                "placement"         : "top",
+                "trigger"           : "hover",
+                "content"           : function(e){
+                    return $(".popover-content").html();
+                },
+                "title"             :function(e){
+                    return $(".popover-title").html();
+                }
+            });
+        });
+        
+    </script>
 </head>
 <body>
     <section id="headerSection">
@@ -23,5 +53,12 @@
             include_once '_/components/php/slide.php';
         ?>
     </section>
+    <section id="fieldSection">
+        <?php
+            include_once '_/components/php/field.php';
+        ?>
+    </section>
+
+    <div style="min-height: 200px"></div>
 </body>
 </html>
